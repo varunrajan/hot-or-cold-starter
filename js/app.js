8ec0eb53837f8334e9f93265ab6b666572197272
varunrajan <varun@thinkful.com>
2015-01-15 11:28:41 -0500

    Set up number generator and log guesses

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -11,7 +10,27 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+  	$(".new").click(function(){
+  	   location.reload();
+  	});
+  // Generate random number between 1 and 100 oupon loading
+    var rightNum = Math.floor((Math.random() * 101) + 1);
+    console.log(rightNum);
 
+
+    $("#guessButton").click(function(event){
+      event.preventDefault();
+      var guess = +$("input#userGuess").val();
+      console.log(guess);
+
+      if (isNaN(guess)){
+        alert("Come on, bruh, that's not a number!");
+      };
+//      else if(Math.abs(rightNum - guess) === 0) {
+//        $("h2#feedback").replace()
+//      }
+
+    });
 });
 
 
