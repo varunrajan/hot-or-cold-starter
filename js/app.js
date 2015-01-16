38d74deaa3ac7598e2f6117b3a61fe2f1a99e235
varunrajan <varun@thinkful.com>
2015-01-16 18:55:33 -0500

    Established conditions for corresponding guesses

--- js/app.js
+++ js/app.js
@@ -22,13 +22,28 @@ $(document).ready(function(){
       event.preventDefault();
       var guess = +$("input#userGuess").val();
       console.log(guess);
+      var difference = Math.abs(rightNum - guess);
+      console.log(difference);
+
 
       if (isNaN(guess)){
         alert("Come on, bruh, that's not a number!");
+      }
+      else if(difference === 0) {
+        console.log("Good job, bruh");
+      }
+      else if(difference > 50){
+        console.log("I can't see you when you're all the way in Antarctica.");
+      }
+      else if(difference <= 50 && difference > 30){
+        console.log("You look like you need a beanie.");
+      }
+      else if(difference <= 30 && difference > 10){
+        console.log("Ooh, heating up. You can probably change into shorts!");
+      }
+      else if(difference <= 10 && difference > 0){
+        console.log("YO DON'T FLY TOO CLOSE TO THE SUN YOUR WINGS WILL BURN OFF");
       };
-//      else if(Math.abs(rightNum - guess) === 0) {
-//        $("h2#feedback").replace()
-//      }
 
     });
 });
