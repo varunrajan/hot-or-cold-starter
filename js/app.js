992c70ef143680d3dcd7f85c61269d313ed40a27
varunrajan <varun@thinkful.com>
2015-01-17 03:56:28 -0500

    Compare each subsequent guess to previous guess

--- js/app.js
+++ js/app.js
@@ -35,27 +35,54 @@ $(document).ready(function(){
         alert("The rules say guess '1 to 100.' Did you read the rules? Idiot.");
       }
       //everything else works here
+
       else {
           $("span#count").replaceWith("<span id='count'>"+(++count)+"</span>");
           $("ul#guessList").append("<li>"+guess+"</li>");
+        var mostRecent = parseInt($("li").last().text());
+        var lastGuess = parseInt($("li").last().prev().text());
+        var differenceNow = Math.abs(rightNum - mostRecent);
+        var differenceLast = Math.abs(rightNum - lastGuess);  
+
+        function sinceLast(){
+          if (count>0){
+            if (differenceNow > differenceLast){
+              $("h2#feedback").append("<h5>Real talk, you're colder than before</h5>");
+            }
+            else if (differenceNow < differenceLast) {
+              $("h2#feedback").append("<h5>Hmm, closer than last time.</h5>");
+            } 
+            else if (differenceNow === differenceLast && mostRecent !== lastGuess){
+              $("h2#feedback").append("<h5>Interesting...You're about the same.</h5>");
+            } 
+            else if (differenceNow === differenceLast && mostRecent === lastGuess){
+              $("h2#feedback").append("<h5>What are you, stupid? You just guessed that.</h5>");
+            };
+          };
+        };
 
           if (isNaN(guess)){
             alert("Come on, bruh, that's not a number!");
           }
           else if(difference === 0) {
-            $("h2#feedback").replaceWith("<h2 id='feedback'>Good job, bruh</h2>")
+            $("h2#feedback").replaceWith("<h2 id='feedback'>Good job, bruh</h2>");
+            sinceLast();
           }
           else if(difference > 50){
-            $("h2#feedback").replaceWith("<h2 id='feedback'>I can't see you when you're all the way in Antarctica.</h2>")
+            $("h2#feedback").replaceWith("<h2 id='feedback'>I can't see you when you're all the way in Antarctica.</h2>");
+            sinceLast();
           }
           else if(difference <= 50 && difference > 30){
-            $("h2#feedback").replaceWith("<h2 id='feedback'>You look like you need a beanie.</h2>")
+            $("h2#feedback").replaceWith("<h2 id='feedback'>You look like you need a beanie.</h2>");
+            sinceLast();
           }
           else if(difference <= 30 && difference > 10){
-            $("h2#feedback").replaceWith("<h2 id='feedback'>Ooh, heating up. You can probably change into shorts!</h2>")
+            $("h2#feedback").replaceWith("<h2 id='feedback'>Ooh, it's warm. You can probably change into shorts!</h2>");
+            sinceLast();
           }
           else if(difference <= 10 && difference > 0){
-            $("h2#feedback").replaceWith("<h2 id='feedback'>YO DON'T FLY TOO CLOSE TO THE SUN YOUR WINGS WILL BURN OFF</h2>")
+            $("h2#feedback").replaceWith("<h2 id='feedback'>YO DON'T FLY TOO CLOSE TO THE SUN YOUR WINGS WILL BURN OFF</h2>");
+            sinceLast();
       };
     };
     });
