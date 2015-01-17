3aacc63f829f7d8f025f6e3dc8f6295d7adda542
varunrajan <varun@thinkful.com>
2015-01-17 00:07:33 -0500

    Add newGame function and reset counter

--- js/app.js
+++ js/app.js
@@ -10,46 +10,52 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
-  	$(".new").click(function(){
-  	   location.reload();
-  	});
+
+    $(".new").click(function(){newGame()})
+    function newGame(){
+  	   count = 0;
+       $("span#count").replaceWith("<span id='count'>"+count+"</span>");
+       $("ul").empty();
+  	};
+
+
   // Generate random number between 1 and 100 oupon loading
     var rightNum = Math.floor((Math.random() * 101) + 1);
     console.log(rightNum);
-
+    var count = parseInt($("span#count").text());
 
     $("#guessButton").click(function(event){
       event.preventDefault();
       var guess = +$("input#userGuess").val();
-      console.log(guess);
       var difference = Math.abs(rightNum - guess);
-      console.log(difference);
-      var count = parseInt($("span#count").text())+1;
 
+
+      //make sure they have to insert an appropriate number according to the rules
       if (guess < 1 || guess > 100){
         alert("The rules say guess '1 to 100.' Did you read the rules? Idiot.");
       }
+      //everything else works here
       else {
-      $("span#count").replaceWith("<span id='count'>"+count+"</span>");
+          $("span#count").replaceWith("<span id='count'>"+(++count)+"</span>");
+          $("ul#guessList").append("<li>"+guess+"</li>");
 
-
-      if (isNaN(guess)){
-        alert("Come on, bruh, that's not a number!");
-      }
-      else if(difference === 0) {
-        $("h2#feedback").replaceWith("<h2 id='feedback'>Good job, bruh</h2>")
-      }
-      else if(difference > 50){
-        $("h2#feedback").replaceWith("<h2 id='feedback'>I can't see you when you're all the way in Antarctica.</h2>")
-      }
-      else if(difference <= 50 && difference > 30){
-        $("h2#feedback").replaceWith("<h2 id='feedback'>You look like you need a beanie.</h2>")
-      }
-      else if(difference <= 30 && difference > 10){
-        $("h2#feedback").replaceWith("<h2 id='feedback'>Ooh, heating up. You can probably change into shorts!</h2>")
-      }
-      else if(difference <= 10 && difference > 0){
-        $("h2#feedback").replaceWith("<h2 id='feedback'>YO DON'T FLY TOO CLOSE TO THE SUN YOUR WINGS WILL BURN OFF</h2>")
+          if (isNaN(guess)){
+            alert("Come on, bruh, that's not a number!");
+          }
+          else if(difference === 0) {
+            $("h2#feedback").replaceWith("<h2 id='feedback'>Good job, bruh</h2>")
+          }
+          else if(difference > 50){
+            $("h2#feedback").replaceWith("<h2 id='feedback'>I can't see you when you're all the way in Antarctica.</h2>")
+          }
+          else if(difference <= 50 && difference > 30){
+            $("h2#feedback").replaceWith("<h2 id='feedback'>You look like you need a beanie.</h2>")
+          }
+          else if(difference <= 30 && difference > 10){
+            $("h2#feedback").replaceWith("<h2 id='feedback'>Ooh, heating up. You can probably change into shorts!</h2>")
+          }
+          else if(difference <= 10 && difference > 0){
+            $("h2#feedback").replaceWith("<h2 id='feedback'>YO DON'T FLY TOO CLOSE TO THE SUN YOUR WINGS WILL BURN OFF</h2>")
       };
     };
     });
